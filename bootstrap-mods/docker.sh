7793f0f7217f144c46f7f4eeb336e59234a60e6d
manasdk <manas@stackstorm.com>
2015-11-05 15:28:03 -0800

    only include trusty tar apt repos

--- bootstrap-mods/docker.sh
+++ bootstrap-mods/docker.sh
@@ -5,14 +5,8 @@
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 cat <<DOCKER_LIST>> /etc/apt/sources.list.d/docker.list
-# Ubuntu Precise
-deb https://apt.dockerproject.org/repo ubuntu-precise main
 # Ubuntu Trusty
 deb https://apt.dockerproject.org/repo ubuntu-trusty main
-# Ubuntu Vivid
-deb https://apt.dockerproject.org/repo ubuntu-vivid main
-# Ubuntu Wily
-deb https://apt.dockerproject.org/repo ubuntu-wily main
 DOCKER_LIST
 
 apt-get -y update
